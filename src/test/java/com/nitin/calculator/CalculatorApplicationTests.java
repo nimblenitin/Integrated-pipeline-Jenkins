516164209bb607184f6f9a8a2fa86fa8133aa106
nimblenitin <naidunitin1@gmail.com>
2021-09-10 15:01:54 +0000

    first commit

---
+++ src/test/java/com/nitin/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nitin.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
